ee7deff92d086b9e8579f6a502c345336766c551
rakshana-it21 <95132051+rakshana-it21@users.noreply.github.com>
2025-06-25 23:15:39 +0530

    Add files via upload

---
+++ Check_identical.java
@@ -0,0 +1,7 @@
+   boolean isIdentical(Node r1, Node r2) {
+        if(r1==null&&r2==null) return true;
+        if(r1==null||r2==null) return false;
+       
+        return (r1.data==r2.data)&& isIdentical(r1.left,r2.left)&&isIdentical(r1.right,r2.right);
+        
+    }
